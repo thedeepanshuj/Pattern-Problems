d6cbc9d58a548d403f4f54e3662371767d64c74f
Deepanshu Jain <deepanshujain@Deepanshus-MacBook-Air.local>
2017-09-16 13:16:42 +0530

    pattern4

---
+++ pattern4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+
+  for(int row=n; row>=1; row--){
+    for(int col=1; col<=row; col++){
+      cout<<"1 ";
+    }
+    cout<<endl;
+  }
+}
